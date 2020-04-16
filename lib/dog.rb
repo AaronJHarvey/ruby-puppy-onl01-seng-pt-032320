8629cb2da7b1bbcaea8d032d53a8dac57e101caf
Aaron J Harvey <harvey.aaronj@gmail.com>
2020-04-16 02:28:48 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,24 +1,22 @@
-class Dog 
-  
+class Dog
+
+  attr_accessor :name
+
   @@all = []
-attr_accessor :name 
 
-def initialize(name)
-  @name = name
-  @@all << self
-end
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
 
-def self.all 
- @@all 
-end
+  def self.all
+    @@all.each do |dog|
+      puts dog.name
+    end
+  end
 
-def self.clear_all
-@@all.clear
-end
-
-def self.print_all
-puts @@all
-end
-
+  def self.clear_all
+    @@all.clear
+  end
 
 end
